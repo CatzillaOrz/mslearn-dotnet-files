8c0c5a4708fe4047ff5f89150cd0c1549ca2cb8e
catzillaorz <catzillaorz@outlook.com>
2022-04-13 15:54:00 +0800

    feat: 9-exercise-read-write-files

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
-﻿var currentDirectory = Directory.GetCurrentDirectory();
+﻿using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);   // Add this line of code
+Directory.CreateDirectory(salesTotalDir);
 
 var salesFiles = FindFiles(storesDirectory);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesTotal = CalculateSalesTotal(salesFiles);
 
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
 IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
@@ -27,5 +26,28 @@ IEnumerable<string> FindFiles(string folderName)
             salesFiles.Add(file);
         }
     }
+
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData(double Total);
